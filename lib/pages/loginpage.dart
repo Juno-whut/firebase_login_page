98417274a543c58f8315aa92f9d6d0a281574056
Juno-whut <owenrupnow17@gmail.com>
2024-11-10 12:10:26 -0800

    added google Fonts plugin and changed the text style of 'Hello Again' to a googlefont

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,6 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_login_page/service/firestore.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -10,8 +12,28 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // text controllers
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
   // firestore
   final FirestoreService firestoreService = FirestoreService();
+
+  // sign user in 
+  Future signIn() async {
+    await FirebaseAuth.instance.signInWithEmailAndPassword(
+      email: _emailController.text.trim(), 
+      password: _passwordController.text.trim(),
+    );
+  }
+
+  // dispose text controllers
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,67 +41,114 @@ class _LoginPageState extends State<LoginPage> {
         backgroundColor: Colors.grey[300],
 
         body:  SafeArea(
-            child: Column(
-                children: [
-                    // welcom message
-                    const SizedBox(height: 25),
-                    const Text("Hello Again",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                    )
-                    ),
-                    const SizedBox(height: 10),
-                    const Text("You've been missed!", 
-                    style: TextStyle(
-                        fontSize: 20,
-                    )
-                    ),
-
-                    const SizedBox(height: 20),
-                    // email text field
-                    Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 25),
-                        child: Container(
-                            decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                border: Border.all(color: Colors.white),
-                                borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 20.0),
-                              child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'Email',
-                                  )
-                              ),
-                            ),
+            child: SingleChildScrollView(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                      // welcom message
+                      const SizedBox(height: 50),
+                      Text(
+                        "Hello Again",
+                        style: GoogleFonts.bebasNeue(
+                          fontSize: 52,
+                          fontWeight: FontWeight.bold,
                         ),
-                    ),
-
-                    const SizedBox(height: 20),
-                    // password text field
-                    Padding(
+                      ),
+                      const SizedBox(height: 10),
+                      const Text("You've been missed!", 
+                      style: TextStyle(
+                          fontSize: 20,
+                      )
+                      ),
+              
+                      const SizedBox(height: 20),
+                      // email text field
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  border: Border.all(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 20.0),
+                                child: TextField(
+                                  controller: _emailController,
+                                    decoration: const InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'Email',
+                                    )
+                                ),
+                              ),
+                          ),
+                      ),
+              
+                      const SizedBox(height: 25),
+                      // password text field
+                      Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 25),
+                          child: Container(
+                              decoration: BoxDecoration(
+                                  color: Colors.grey[200],
+                                  border: Border.all(color: Colors.white),
+                                  borderRadius: BorderRadius.circular(15),
+                              ),
+                              child: Padding(
+                                padding: const EdgeInsets.only(left: 20.0),
+                                child: TextField(
+                                  controller: _passwordController,
+                                  obscureText: true,
+                                    decoration: const InputDecoration(
+                                        border: InputBorder.none,
+                                        hintText: 'Password',
+                                    )
+                                ),
+                              ),
+                          )
+                      ),
+                      const SizedBox(height: 15),
+                      // Sign in button
+                      Padding(
                         padding: const EdgeInsets.symmetric(horizontal: 25),
-                        child: Container(
+                        child: GestureDetector(
+                          onTap: signIn,
+                          child: Container(
+                            padding: const EdgeInsets.all(20),
                             decoration: BoxDecoration(
-                                color: Colors.grey[200],
-                                border: Border.all(color: Colors.white),
-                                borderRadius: BorderRadius.circular(15),
-                            ),
-                            child: const Padding(
-                              padding: EdgeInsets.only(left: 20.0),
-                              child: TextField(
-                                  decoration: InputDecoration(
-                                      border: InputBorder.none,
-                                      hintText: 'Password',
-                                  )
+                              color: Colors.deepPurple,
+                              borderRadius: BorderRadius.circular(15),
+                              ),
+                            child: const Center(
+                              child: Text(
+                                "Sign In",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 18,
+                                ),
                               ),
                             ),
+                          ),
                         )
-                    )
-                ],
+                      ),
+                      const SizedBox(height: 15),
+                      // not a member? register now
+                      const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Text("Not a member?",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold
+                          )),
+                          Text("Register Now",
+                          style: TextStyle(
+                            color: Colors.blue,
+                            fontWeight: FontWeight.bold
+                          ))
+                        ],)
+                    ],
+              ),
             )
         )
     );
