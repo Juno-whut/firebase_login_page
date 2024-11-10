6ddd92ae2841e1ea0f4046eab37dc322b6814921
Juno-whut <owenrupnow17@gmail.com>
2024-11-10 11:06:17 -0800

    added some UI changes, A welcome back message, an email field, and a password field

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -16,17 +16,72 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text(
-          'Login Page',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 32
-          ),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.blueGrey,
-      ),
+        backgroundColor: Colors.grey[300],
+
+        body:  SafeArea(
+            child: Column(
+                children: [
+                    // welcom message
+                    const SizedBox(height: 25),
+                    const Text("Hello Again",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 24,
+                    )
+                    ),
+                    const SizedBox(height: 10),
+                    const Text("You've been missed!", 
+                    style: TextStyle(
+                        fontSize: 20,
+                    )
+                    ),
+
+                    const SizedBox(height: 20),
+                    // email text field
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: TextField(
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Email',
+                                  )
+                              ),
+                            ),
+                        ),
+                    ),
+
+                    const SizedBox(height: 20),
+                    // password text field
+                    Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 25),
+                        child: Container(
+                            decoration: BoxDecoration(
+                                color: Colors.grey[200],
+                                border: Border.all(color: Colors.white),
+                                borderRadius: BorderRadius.circular(15),
+                            ),
+                            child: const Padding(
+                              padding: EdgeInsets.only(left: 20.0),
+                              child: TextField(
+                                  decoration: InputDecoration(
+                                      border: InputBorder.none,
+                                      hintText: 'Password',
+                                  )
+                              ),
+                            ),
+                        )
+                    )
+                ],
+            )
+        )
     );
   }
 }
